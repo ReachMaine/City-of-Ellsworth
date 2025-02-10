d25a42cc3063792699bc0cde74d0d54f7d56ea87
Linda "zig" Speight <zig.speight@gmail.com>
2025-02-10 11:08:31 -0500

    put background container on all pages

--- ellsworthCity/page.php
+++ ellsworthCity/page.php
@@ -1,47 +1,101 @@
 <?php get_header(); ?>
 
+
+
 <?php while ( have_posts() ) : the_post(); ?>
 
+
+
 	<?php // PAGE CONTENT BEFORE
+
 	get_template_part( 'components/page-content-before' ); ?>
 
+
+
 	<!-- PAGE CONTENT : begin -->
+
 	<div id="page-content"<?php if ( lsvr_get_field( 'meta_content_boxed', 'single' ) === 'disable' ) { echo ' class="m-no-boxes"'; } ?>>
 
-		<?php if ( lsvr_get_field( 'meta_content_boxed', 'single' ) === 'single' ) : ?>
+
+
+		<?php /* if ( lsvr_get_field( 'meta_content_boxed', 'single' ) === 'single' ) : zig - alway use background */ ?>
+
+
 
 			<div class="c-content-box">
+
 				<div class="page-content-inner">
+
+
+
 					<?php the_content(); ?>
+
+					<?php wp_link_pages(); ?>
+
+
+
 				</div>
+
 			</div>
 
-		<?php else: ?>
+
+
+		<?php /* else: ?>
+
+
 
 			<div class="page-content-inner">
 
+
+
 				<?php the_content(); ?>
 
-			    <?php if ( comments_open() ) : ?>
-			    <!-- ARTICLE COMMENTS : begin -->
-				<div class="article-comments" id="comments">
-					<div class="c-content-box">
-					  <?php comments_template(); ?>
-					</div>
-				</div>
-			    <!-- ARTICLE COMMENTS : end -->
-			    <?php endif; ?>
+				<?php wp_link_pages(); ?>
+
+
 
 			</div>
 
-		<?php endif; ?>
+
+
+		<?php endif;*/ ?>
+
+
+
+	    <?php if ( comments_open() ) : ?>
+
+	    <!-- ARTICLE COMMENTS : begin -->
+
+		<div class="article-comments" id="comments">
+
+			<div class="c-content-box">
+
+			  <?php comments_template(); ?>
+
+			</div>
+
+		</div>
+
+	    <!-- ARTICLE COMMENTS : end -->
+
+	    <?php endif; ?>
+
+
 
 	</div>
+
 	<!-- PAGE CONTENT : end -->
 
+
+
 	<?php // PAGE CONTENT AFTER
+
 	get_template_part( 'components/page-content-after' ); ?>
 
+
+
 <?php endwhile; ?>
+
+
 
 <?php get_footer(); ?>
